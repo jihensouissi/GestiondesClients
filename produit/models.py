2f82ffab1eb9333eccbc147fe434dbbce4c8458d
asus tuf <jihen.souissi@esprit.tn>
2023-05-12 01:14:25 +0200

    commit

--- produit/models.py
+++ produit/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Produit(models.Model):
-    nom=models.chartFiels(max_length=200,null=True)
+    nom=models.CharField(max_length=200,null=True)
     prix=models.FloatField(null=True)
