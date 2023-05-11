117a3e7b429e6c04a9f7bb128b630aa80f23d2c2
asus tuf <jihen.souissi@esprit.tn>
2023-05-11 02:40:38 +0200

    Initial commit

---
+++ commande/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from  django.http import HttpResponse
+
+# Create your views here.
+def list_commande (request):
+    return render(request,'commande\list_commande.html')
